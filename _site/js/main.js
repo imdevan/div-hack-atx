6c97d3371bd9c645b663f319211ef4421b2e1ede
bluyam <bluyam@tamu.edu>
2015-09-13 04:06:13 -0500

    added all api urls

--- _site/js/main.js
+++ _site/js/main.js
@@ -8,7 +8,6 @@ function ajaxRequest(id, apiurl) {
   crossDomain: true,
     dataType: "jsonp",
     success: function (response) {
-			console.log(id);
       var t = response.results.collection1;
       for(var i = 0; i < t.length; i++) {
         var prop = t[i].property1;
@@ -20,26 +19,26 @@ function ajaxRequest(id, apiurl) {
 var api = {
   huffpost:  "https://www.kimonolabs.com/api/1vnzmulw?apikey=82g96zzCQGXPX0Z3htZgjT74Vc7hvFCN",
   verge: "https://www.kimonolabs.com/api/2dje4o7w?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
-  mashable:  "https://www.kimonolabs.com/api/4zgbid62?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC"
+  mashable:  "https://www.kimonolabs.com/api/4zgbid62?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  imdiversity: "https://www.kimonolabs.com/api/57hown3e?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  newser: "https://www.kimonolabs.com/api/drj6e7zo?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  agsci: "https://www.kimonolabs.com/api/6vm998xm?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  diversityinc: "https://www.kimonolabs.com/api/7te7qreq?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  entrepreneur: "https://www.kimonolabs.com/api/97nzx0te?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  scholarships: {
+    indian:  "https://www.kimonolabs.com/api/c96ihge8?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+    african: "https://www.kimonolabs.com/api/cpaf5bj8?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+    middleeastern: "https://www.kimonolabs.com/api/akv3o700?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+    latino: "https://www.kimonolabs.com/api/c3oxsuua?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+    asian: "https://www.kimonolabs.com/api/1z3d9pao?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+    women: "https://www.kimonolabs.com/api/1wtmqk46?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+    lgbt: "https://www.kimonolabs.com/api/94qw18w8?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC"
+  }
 }
 
-$('.article-list--title').on('click', function () {
-  for(var key in api) {
-    // console.log(key);
-  }
-});
 
 ajaxRequest('#news-list', api.huffpost);
 
 ajaxRequest('#stem-list', api.verge);
 
 ajaxRequest('#scholarship-list', api.mashable);
-
-var titlename = '#news-list-title';
-// refreshCol function ()
-$(titlename).click(function (  ) {
-	var listname = titlename.substr(0,titlename.length-6);
-	var correspondinglist = $(listname);
-	correspondinglist.empty();
-	ajaxRequest(listname, api.mashable);
-});
--- js/selector.js
+++ js/selector.js
@@ -1,11 +1,5 @@
 var v, newsreader = {
     vars: {
-        // api variables
-        apis: {
-          huffpost:  "https://www.kimonolabs.com/api/1vnzmulw?apikey=82g96zzCQGXPX0Z3htZgjT74Vc7hvFCN",
-          verge: "https://www.kimonolabs.com/api/2dje4o7w?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
-          mashable:  "https://www.kimonolabs.com/api/4zgbid62?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC"
-        },
         // ui elements
         titles: $('.article-list--title'),
         lists: $('.article-list'),
@@ -22,7 +16,6 @@ var v, newsreader = {
         updating_index: 0
     },
     init: function () {
-        console.log('init');
         v = this.vars;
         this.bindUI();
     },
@@ -33,18 +26,12 @@ var v, newsreader = {
         v.titles.click(function () {
             v.updating_index = v.titles.index($(this));
             v.selectors.show(v.updating_index);
-            console.log(v.updating_index);
         });
 
         v.selector_buttons.click(function() {
-            // apiToLoad = $(this).val(); // api to loads
-            // console.log(apiToLoad);
-            // v.lists[updating_index]; // is the list we update
-            // updating_index = v.selector_buttons.index($(this));
-            console.log(v.updating_index);
             $(v.selectors).removeClass("open");
             $(v.lists[v.updating_index]).empty();
-            // ajaxRequest(v.lists[v.updating_index], v.apis[v.updating_index]);
+            ajaxRequest(v.lists[v.updating_index], api[$(this).attr("data-id")]);
         });
     }
 }
