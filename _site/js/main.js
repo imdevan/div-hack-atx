aec05cfc0a26325d55f72643e9da497db0d2e270
Devan Huapaya <huapayadevan@gmail.com>
2015-09-12 19:29:03 -0500

    changes to ajax

--- _site/js/main.js
+++ _site/js/main.js
@@ -9,10 +9,29 @@
       var inner = t[i].property1;
       console.log(inner);
       //<li class="article"><a href=""></a></li>
-      $("#news").append("<li class=\"article\"><a href=\""+ inner["href"] + "\">" + inner["text"] + "</a></li>" );
+      $("#newscolumn").append("<li class=\"article\"><a href=\""+ inner["href"] + "\">" + inner["text"] + "</a></li>" );
     }
   },
   error: function (xhr, status) {
     //handle errors
   }
-})
+});
+
+
+  $.ajax({
+  url:"https://www.kimonolabs.com/api/2dje4o7w?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
+  crossDomain: true,
+  dataType: "jsonp",
+  success: function (response) {
+    var t = response.results.collection1;
+    for(var i = 0; i < t.length; i++) {
+      var inner = t[i].property1;
+      console.log(inner);
+      //<li class="article"><a href=""></a></li>
+      $("#stemcolumn").append("<li class=\"article\"><a href=\""+ inner["href"] + "\">" + inner["text"] + "</a></li>" );
+    }
+  },
+  error: function (xhr, status) {
+    //handle errors
+  }
+});
