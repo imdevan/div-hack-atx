6de22720afba8710ecc943ef1fe03c7c6e5e209d
Devan Huapaya <huapayadevan@gmail.com>
2015-09-13 00:05:53 -0500

    resusing code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,24 @@
-  function postArticle(id, href, text) {
-    $(id).append("<a class='article-list--link' href=\""+ href + "\"><li class='article-list--article'>" + text + "</li></a>" );
-  }
+function postArticle(id, href, text) {
+  $(id).append("<a class='article-list--link' href=\""+ href + "\"><li class='article-list--article'>" + text + "</li></a>" );
+}
 
-$.ajax({
-url:"https://www.kimonolabs.com/api/1vnzmulw?apikey=82g96zzCQGXPX0Z3htZgjT74Vc7hvFCN",
-crossDomain: true,
-  dataType: "jsonp",
-  success: function (response) {
-    var t = response.results.collection1;
-    for(var i = 0; i < t.length; i++) {
-      var prop = t[i].property1;
-      postArticle("#news-list", prop["href"], prop["text"]);
-    } 
-  }
-});
-
-
-
-$.ajax({
-url:"https://www.kimonolabs.com/api/2dje4o7w?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
-crossDomain: true,
-dataType: "jsonp",
-  success: function (response) {
-    var t = response.results.collection1;
-    for(var i = 0; i < t.length; i++) {
-      var prop = t[i].property1;
-      postArticle("#stem-list", prop["href"], prop["text"]);
+function ajaxRequest(id, apiurl) {
+  $.ajax({
+  url: apiurl,
+  crossDomain: true,
+    dataType: "jsonp",
+    success: function (response) {
+      var t = response.results.collection1;
+      for(var i = 0; i < t.length; i++) {
+        var prop = t[i].property1;
+        postArticle(id, prop["href"], prop["text"]);
+      } 
     }
-  }
-});
+  });
+}
 
+ajaxRequest('#news-list', "https://www.kimonolabs.com/api/1vnzmulw?apikey=82g96zzCQGXPX0Z3htZgjT74Vc7hvFCN");
 
-$.ajax({
-url:"https://www.kimonolabs.com/api/4zgbid62?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC",
-crossDomain: true,
-dataType: "jsonp",
-  success: function (response) {
-    var t = response.results.collection1;
-    for(var i = 0; i < t.length; i++) {
-      var prop = t[i].property1;
-      postArticle("#scholarship-list", prop["href"], prop["text"]);
-    }
-  }
-});
+ajaxRequest('#stem-list', "https://www.kimonolabs.com/api/2dje4o7w?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC");
+
+ajaxRequest('#scholarship-list', "https://www.kimonolabs.com/api/4zgbid62?apikey=F400P9FjVyuGFjUIs6x4pLLQXD8S8kpC");
